46a89d69e4331a109c507cffc002aeb95d1b2311
Meagan.Barfield <meagan42192@gmail.com>
2026-08-31 18:03:41 -0500

    updated the about page to have classes and then updated the css to make the about page prettier

--- src/components/pages/AboutPage.jsx
+++ src/components/pages/AboutPage.jsx
@@ -1,30 +1,38 @@
 import { FaArrowLeft } from "react-icons/fa";
 import { Link } from "react-router-dom";
-import HomePage from "./HomePage";
 import Header from "../layout/Header";
 
 function AboutPage() {
-
     return (
-        <main>
-            <div>
-                <Header />
-                <Link to="/home">
-                    <p>
-                        <small className="back-text-wrapper">
-                            <FaArrowLeft className="back-icon" />
-                            Back to Home
-                        </small>
-                    </p>
+        <>
+            <Header />
+
+            <main className="about-page">
+                <Link to="/home" className="about-back-link">
+                    <FaArrowLeft className="back-icon" />
+                    Back to Home
                 </Link>
-            </div>
-            <div>
-                <h1>About Next Stop</h1>
-                <p>Next Stop was created to make planning your next adventure simple and exciting. Explore beautiful destinations, discover places you’d love to visit, and keep your travel plans organized all in one place. Whether you’re planning an upcoming getaway, saving ideas for the future, or looking back on past adventures, Next Stop helps you keep track of every journey along the way.</p>
-                <h2>Because the best part of every trip is deciding where to go next.</h2>
-            </div>
-        </main>
+
+                <section className="about-content">
+                    <h1>About Next Stop</h1>
+
+                    <p>
+                        Next Stop was created to make planning your next adventure
+                        simple and exciting. Explore beautiful destinations,
+                        discover places you’d love to visit, and keep your travel
+                        plans organized all in one place. Whether you’re planning
+                        an upcoming getaway, saving ideas for the future, or
+                        looking back on past adventures, Next Stop helps you keep
+                        track of every journey along the way.
+                    </p>
+
+                    <h2>
+                        Because the best part of every trip is deciding where to go next.
+                    </h2>
+                </section>
+            </main>
+        </>
     );
-};
+}
 
 export default AboutPage;
