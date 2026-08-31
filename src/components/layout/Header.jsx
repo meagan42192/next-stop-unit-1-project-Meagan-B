6c921e349863da554fe57ad4328f86f88a2c00e7
Meagan.Barfield <meagan42192@gmail.com>
2026-08-31 17:56:54 -0500

    pulled the header logo from the new logo page

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,12 +1,14 @@
 import NavMenu from './NavMenu';
 import { Link } from 'react-router-dom';
+import logo from '../../data/nextStopLogo';
 
 const Header = () => {
     return (
         <header className="site-header">
-            <div className="header-logo">
+            <div>
                 <img
-                    src="https://ik.imagekit.io/fbon0i47u/nextStopLogo.png" width="200" height="60"
+                    className='header-logo'
+                    src={logo}
                     alt="Next Stop logo"
                 />
             </div>
